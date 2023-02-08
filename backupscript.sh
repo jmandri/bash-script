3cce344eda4fdaa1a83b4b61f1471a76c024cc0a
jmandri <117689421+jmandri@users.noreply.github.com>
2023-02-08 19:50:47 +0100

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Get current date and time for backup directory name
+# Get current date and time for backup directory
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
 # Check if target is remote or local
 if [[ $2 == *"@"* ]]; then
   # Copy to remote target using rsync over ssh
+  ssh "$2" "mkdir -p backups"
   rsync -az --delete --link-dest=$2/current $1 $2:backups/$timestamp
 else
   # Copy to local target using rsync
+  mkdir -p $2/backups
   rsync -az --delete --link-dest=$2/current $1 $2/backups/$timestamp
 fi
 

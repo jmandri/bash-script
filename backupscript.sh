4f7a40f125991c97a6cf5793ca80b5ab58810dbf
jmandri <117689421+jmandri@users.noreply.github.com>
2023-03-09 19:51:45 +0100

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-# Get current date and time for backup directory
+# Check if two arguments are provided
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 source_directory target_directory"
+  exit 1
+fi
+
+# Get current date and time for backup directory name
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
 # Check if target is remote or local
 if [[ $2 == *"@"* ]]; then
   # Copy to remote target using rsync over ssh
-  ssh "$2" "mkdir -p backups"
-  rsync -az --delete --link-dest=$2/current $1 $2:backups/$timestamp
+  ssh -o "StrictHostKeyChecking=no" $2 "mkdir -p backups"
+  rsync -az --delete --link-dest="$2/current" "$1" "$2:backups/$timestamp"
 else
   # Copy to local target using rsync
-  mkdir -p $2/backups
-  rsync -az --delete --link-dest=$2/current $1 $2/backups/$timestamp
+  mkdir -p "$2/backups"
+  rsync -az --delete --link-dest="$2/current" "$1" "$2/backups/$timestamp"
 fi
 
 # Create symlink to latest backup
 if [ -e "$2/current" ]; then
-  rm $2/current
+  rm "$2/current"
 fi
-ln -s $2/backups/$timestamp $2/current
+ln -s "$2/backups/$timestamp" "$2/current"
+
+if [ $? -ne 0 ]; then
+  echo "Failed to create symlink to latest backup."
+  exit 1
+fi
+
+echo "Backup completed successfully."
+exit 0
